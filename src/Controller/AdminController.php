fada5f7503d30d2f564b4b90bb90cfaf2ae5f780
mcoudurier <mathieu.coudurier@gmail.com>
2018-04-21 20:28:31 +0200

    Declare the upload directory path in config

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -46,7 +46,7 @@ class AdminController extends Controller
                 $filesize = filesize($file);
                 $image->setSize($filesize);
                 $image->setName($filename);
-                $file->move('img/upload/', $filename);
+                $file->move($this->getParameter('images_directory'), $filename);
             }
 
             $product = $form->getData();
