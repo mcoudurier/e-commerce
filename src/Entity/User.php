c8f2d05f6ad35d7a7dae0c7d639c6a8a3d2eb7c8
mcoudurier <mathieu.coudurier@gmail.com>
2018-05-15 07:00:13 +0200

    Reduce email column length to 191
    Avoids key too long errors when innodb_large_prefix isn't enabled

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -36,7 +36,7 @@ class User implements UserInterface, \Serializable
     private $password;
 
     /**
-     * @ORM\Column(type="string", length=255, unique=true)
+     * @ORM\Column(type="string", length=191, unique=true)
      */
     private $email;
 
